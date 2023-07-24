8176cfc9c450e76d8e6d5ae3817044e339a10ec0
jeffmlb91 <jeffmlb91@gmail.com>
2023-07-23 21:14:56 -0400

    update:google form layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,6 @@ import { ThemedTitleV2 } from "@refinedev/mui";
 
 import { yariga } from "../assets";
 
-
 import { CredentialResponse } from "../interfaces/google";
 
 // Todo: Update your Google Client ID here
@@ -29,7 +28,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -50,40 +49,70 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <Container
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box
-        display="flex"
-        gap="36px"
-        justifyContent="center"
-        flexDirection="column"
+    <Box component="div" sx={{ backgroundColor: "#FCFCFC" }}>
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100vh",
+        }}
       >
-        <ThemedTitleV2
-          collapsed={false}
-          wrapperStyles={{
-            fontSize: "22px",
+        <Box
+          sx={{
+            display: "flex",
             justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
+        >
+          <div>
+            <img src={yariga} alt="Yariga Logo" />
+          </div>
+          <Box mt={4}>
+            <GoogleButton />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+    // <Container
+    //   style={{
+    //     height: "100vh",
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     backgroundColor: "#FCFCFC"
+    //   }}
+    // >
+    //   <Box
+    //     display="flex"
+    //     gap="36px"
+    //     justifyContent="center"
+    //     flexDirection="column"
 
-        <GoogleButton />
+    //   >
+    //     {/* <ThemedTitleV2
+    //       collapsed={false}
+    //       wrapperStyles={{
+    //         fontSize: "22px",
+    //         justifyContent: "center",
+    //       }}
+    //     /> */}
 
-        <Typography align="center" color={"text.secondary"} fontSize="12px">
-          Powered by
-          <img
-            style={{ padding: "0 5px" }}
-            alt="Google"
-            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
-          />
-          Google
-        </Typography>
-      </Box>
-    </Container>
+    //     <GoogleButton />
+
+    //     <Typography align="center" color={"text.secondary"} fontSize="12px">
+    //       Powered by
+    //       <img
+    //         style={{ padding: "0 5px" }}
+    //         alt="Google"
+    //         src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
+    //       />
+    //       Google
+    //     </Typography>
+    //   </Box>
+    // </Container>
   );
 };
